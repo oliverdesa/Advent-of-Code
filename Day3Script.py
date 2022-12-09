1e81b21dc10875b84fc1cde908441e714d9cde9b
Oliver De Sa <oliver.desa@mail.utoronto.ca>
2022-12-08 19:09:44 -0500

    added code to complete part 2 of day 3

--- Day3Script.py
+++ Day3Script.py
@@ -3,19 +3,40 @@ from string import ascii_letters
 with open('Rucksacks.txt') as file:
    data = [i for i in file.read().strip().split('\n')]
 
-
-totalSum = 0
+x = 0
+allGroups = []
+group = []
 for rucksack in data:
-   half = len(rucksack)//2
+   x += 1
+   group.append(set(rucksack))
+   if x == 3:
+      allGroups.append(group)
+      group = []
+      x = 0
    
-   left = set(rucksack[:half])
-   right = set(rucksack[half:])
-
+totalSum = 0
+for group in allGroups:
    for priority, char in enumerate(ascii_letters):
-      if char in left and char in right:
+      if char in group[0] and char in group[1] and char in group[2]:
          totalSum += priority + 1
 
 print(totalSum)
+
+
+
+
+# totalSum = 0
+# for rucksack in data:
+#    half = len(rucksack)//2
+   
+#    left = set(rucksack[:half])
+#    right = set(rucksack[half:])
+
+#    for priority, char in enumerate(ascii_letters):
+#       if char in left and char in right:
+#          totalSum += priority + 1
+
+
 
 
 
